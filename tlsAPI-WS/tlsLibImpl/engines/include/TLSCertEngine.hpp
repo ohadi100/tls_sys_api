25e47c5c00ef33b3274a380bdcf1445dad926fa1
ohadi100 <136633247+ohadi100@users.noreply.github.com>
2024-04-14 02:46:21 +0300

    Update TLSCertEngine.hpp

--- tlsAPI-WS/tlsLibImpl/engines/include/TLSCertEngine.hpp
+++ tlsAPI-WS/tlsLibImpl/engines/include/TLSCertEngine.hpp
@@ -1,38 +1,32 @@
 /**
+ * @file TLSCertEngine.hpp
+ * @brief Defines the TLSCertEngine class which implements the ITLSEngine interface.
  *
- * \copyright
+ * This file contains the definition of the TLSCertEngine class, which provides
+ * functionalities to handle TLS operations such as handshake, sending and receiving
+ * data securely, and managing TLS sessions with OCSP support.
+ *
+ * @copyright
  * (c) 2022, 2023 CARIAD SE, All rights reserved.
  *
  * NOTICE:
+ * All information and materials contained herein, including intellectual and technical
+ * concepts, are the property of CARIAD SE and may be covered by patents, patents in process,
+ * and are protected by trade secret and/or copyright law.
  *
- * All the information and materials contained herein, including the
- * intellectual and technical concepts, are the property of CARIAD SE and may
- * be covered by patents, patents in process, and are protected by trade
- * secret and/or copyright law.
+ * Reproduction, dissemination, modification, distribution, public performance,
+ * public display, or any other use of this source code without the prior written consent
+ * of CARIAD SE is strictly prohibited and in violation of applicable laws.
  *
- * The copyright notice above does not evidence any actual or intended
- * publication or disclosure of this source code, which includes information
- * and materials that are confidential and/or proprietary and trade secrets of
- * CARIAD SE.
- *
- * Any reproduction, dissemination, modification, distribution, public
- * performance, public display of or any other use of this source code and/or
- * any other information and/or material contained herein without the prior
- * written consent of CARIAD SE is strictly prohibited and in violation of
- * applicable laws.
- *
- * The receipt or possession of this source code and/or related information
- * does not convey or imply any rights to reproduce, disclose or distribute
- * its contents or to manufacture, use or sell anything that it may describe
- * in whole or in part.
+ * Possession of this source code does not convey or imply any rights to reproduce,
+ * disclose, or distribute its contents, or to manufacture, use, or sell anything that it
+ * may describe, in whole or in part.
  */
 
 #ifndef SACCESSLIB_TLSCERTENGINE_HPP
 #define SACCESSLIB_TLSCERTENGINE_HPP
 
 #include <functional>
-
-#include <vwgtypes.h>
 #include <cstdint>
 #include <memory>
 #include <string>
@@ -47,32 +41,33 @@ namespace tls
 {
 namespace impl
 {
+
 /**
- * \enum TLSEngineErrors
+ * @class TLSCertEngine
+ * @brief TLS Certificate Engine for managing secure communications.
  *
- * \brief Generic error values for any tls engine.
+ * TLSCertEngine implements ITLSEngine to provide methods for TLS communication using
+ * certificates. It supports operations like SSL handshake, sending and receiving data,
+ * and session management.
  */
 class TLSCertEngine : public ITLSEngine
 {
 public:
     /**
-     * \brief Constructor.
+     * @brief Constructs a TLSCertEngine with a stream and optional time checks.
      *
-     * \param[in] stream the underlying IOStreamIf used by the engine to perform actual input/output.
-     * \param[in] checkTime do the time check in addition to the certificate validity check. This check will verify if
-     * the certificate check time. This check can be omitted, by using null for this parameter.
+     * @param stream The underlying IO stream used by the engine to perform I/O operations.
+     * @param checkTime Specifies time checking parameters to validate certificate validity against.
      */
     TLSCertEngine(std::shared_ptr<IOStreamIf> const& stream, const TimeCheckTime& checkTime);
 
     /**
-     * \brief Constructor.
+     * @brief Constructs a TLSCertEngine with OCSP support.
      *
-     * \param[in] stream the underlying IOStreamIf used by the engine to perform actual input/output.
-     * \param[in] checkTime do the time check in addition to the certificate validity check. This check will verify if
-     * the certificate check time. This check can be omitted, by using null for this parameter.
-     * \param[in] ocspHandler Instance that implements ITLSOcspHandler interface that define methods to handle
-     * ocsp requests and responses.
-     * \param[in] ocspTimeoutMs OCSP timeout in milliseconds.
+     * @param stream The underlying IO stream used by the engine.
+     * @param checkTime Time check settings for certificate validation.
+     * @param ocspHandler Handler for OCSP requests and responses.
+     * @param ocspTimeoutMs Timeout in milliseconds for OCSP responses.
      */
     TLSCertEngine(std::shared_ptr<IOStreamIf> const&      stream,
                   const TimeCheckTime&                    checkTime,
@@ -80,134 +75,125 @@ public:
                   const uint32_t                          ocspTimeoutMs);
 
     /**
-     * \brief Destructor.
+     * @brief Destructor.
      */
     virtual ~TLSCertEngine() = default;
 
     /**
-     * \brief Performs the TLS handshake, according to the arguments provided in the constructor.
+     * @brief Performs the SSL handshake.
      *
-     * \return RC_TLS_SUCCESSFUL if succeeded, otherwise an error code.
+     * @return TLS engine error code indicating the result of the handshake.
      */
     virtual TLSEngineError DoSSLHandshake() = 0;
 
     /**
-     * \brief Sends a buffer to the other side.
+     * @brief Sends data securely over the TLS connection.
      *
-     * \param[in] buffer an unencrypted buffer of size 'length', which will be encrypted and sent through the
-     * underlying (inheriting) TLS engine. This argument must be pre-allocated (either statically or
-     * dynamically) by the callee.
-     * \param[in] bufLength length of unencrypted buffer.
-     * \param[out] actualLength length of unencrypted buffer actually sent.
-     *
-     * \return RC_TLS_SUCCESSFUL if succeeded, otherwise an error code.
+     * @param buffer Pointer to the data buffer.
+     * @param bufLength Length of the buffer.
+     * @param actualLength Length of the data actually sent.
+     * @return TLS engine error code.
      */
     virtual TLSEngineError Send(const uint8_t* buffer, int32_t bufLength, int32_t& actualLength) = 0;
 
     /**
-     * \brief Receives a buffer from the other side.
+     * @brief Receives data securely over the TLS connection.
      *
-     * \param[in] bufLength - length of unencrypted buffer to read.
-     * \param[in] actualLength - length of unencrypted buffer actually read.
-     * \param[out] buffer - a buffer of size 'length' to receive the data. 'buffer' should be pre-allocated (either
-     * statically or dynamically) by the callee.
-     *
-     * \return RC_TLS_SUCCESSFUL if succeeded, otherwise an error code.
+     * @param buffer Pointer to the buffer to store received data.
+     * @param bufLength Length of the buffer.
+     * @param actualLength Length of the data actually received.
+     * @return TLS engine error code.
      */
     virtual TLSEngineError Receive(uint8_t* buffer, int32_t bufLength, int32_t& actualLength) = 0;
 
     /**
-     * \brief Sets blocking/non-blocking mode for the stream. Blocking by default.
+     * @brief Sets the blocking mode for the underlying stream.
      *
-     * \param[in] blocking the new mode.
-     *
-     * \return RC_TLS_SUCCESSFUL if succeeded, otherwise an error code.
+     * @param blocking True for blocking mode, false for non-blocking.
+     * @return TLS engine error code.
      */
     virtual TLSEngineError SetBlocking(bool blocking);
 
     /**
-     * \brief Sends a "close notify" alert to the peer.
+     * @brief Initiates a shutdown of the TLS session by sending a close notify alert.
+     *
+     * @return TLS engine error code.
      */
     virtual TLSEngineError Shutdown() = 0;
 
-/**
- * \def use the define <b> TLSAPI_WITH_DROP_SUPPORT </b> to generate the special library for the MOD socks
- * implementation. Only for the MOD socks implementation the sockets with droppable shall be present. The default
- * the library implementation shall not provide droppable sockets.
- */
 #ifdef TLSAPI_WITH_DROP_SUPPORT
     /**
-     * \brief Initiates drop TLS with the peer.
+     * @brief Initiates the TLS drop process.
+     *
+     * This is only available if TLSAPI_WITH_DROP_SUPPORT is defined.
+     * @return TLS engine error code.
      */
     virtual TLSEngineError DropTLS() override;
 #endif
 
     /**
-     * \brief Gets the accompanying IOStream.
+     * @brief Returns the IOStream associated with the TLS engine.
      *
-     * \return the accompanying IOStream.
+     * @return Shared pointer to the IOStream.
      */
-    const std::shared_ptr<IOStream> GetIOStream() const;
+    const std::shared_ptr<IOStreamIf> GetIOStream() const;
 
     /**
-     * \brief Gets the OCSP Handler object.
+     * @brief Returns the OCSP handler used by the TLS engine.
      *
-     * \return a shared pointer reference to the handler object.
+     * @return Shared pointer to the OCSP handler.
      */
     const std::shared_ptr<ITLSOcspHandler>& GetOcspHandler() const;
 
     /**
-     * \brief Gets the OCSP response receive timeout in milliseconds.
+     * @brief Returns the timeout for OCSP responses.
      *
-     * \return an unsigned integer which contains the timeout value in milliseconds.
+     * @return Timeout in milliseconds.
      */
     uint32_t GetOcspTimeout() const;
 
     /**
-     * \brief Checks the authentication of the system time, by comparing to m_checkTime,
-     * should be called at the handshake time.
+     * @brief Validates the system time against the expected time settings.
      *
-     * \return  RC_TLS_ENGINE_SUCCESSFUL if |(m_checkTime.expectedTime - systemTime)| <=
-     * |m_checkTime.permittedDeviation| otherwise RC_TLS_ENGINE_AUTHENTIC_TIMECHECK_FAILED or another error.
+     * @return TLS engine error code based on the time check result.
      */
     virtual TLSEngineError CheckAuthenticTimeCheck() const;
 
     /**
-     * \brief Gets engine's used cipher suite group.
+     * @brief Returns the cipher suite settings used by the engine.
      *
-     * \return The used cipher suite group.
+     * @return Cipher suite settings.
      */
     virtual TLSCipherSuiteUseCasesSettings GetCipherSuiteUseCase() const;
 
     /**
-     * \brief Gets engine's revocation check enable flag.
+     * @brief Checks if revocation checking is enabled.
      *
-     * \return the revocation check enable flag.
+     * @return True if enabled, otherwise false.
      */
     bool GetRevocationCheckEnable() const;
 
     /**
-     * \brief Gets indication if the hard fail fallback mechanism is active
+     * @brief Checks if the hard fail fallback mechanism is active.
      *
-     * \return the hard fail fallback mechanism status
+     * @return True if active, otherwise false.
      */
     bool IsHardFailFallbackMechanismActive() const;
 
 #ifndef UNIT_TEST
 protected:
 #endif
-    std::shared_ptr<IOStreamIf>      m_stream;
-    TimeCheckTime const              m_checkTime;
-    TLSCipherSuiteUseCasesSettings   m_cipherSuiteUseCase;
-    std::shared_ptr<ITLSOcspHandler> m_ocspHandler;
-    uint32_t const                   m_ocspTimeoutMs;
-    bool                             m_revocationCheckEnabled;
-    std::string                      m_connectionLoggingName;
+    std::shared_ptr<IOStreamIf>      m_stream; ///< Stream used for I/O operations.
+    TimeCheckTime const              m_checkTime; ///< Time check settings.
+    TLSCipherSuiteUseCasesSettings   m_cipherSuiteUseCase; ///< Cipher suite configuration.
+    std::shared_ptr<ITLSOcspHandler> m_ocspHandler; ///< OCSP handler for certificate validation.
+    uint32_t const                   m_ocspTimeoutMs; ///< Timeout for OCSP responses.
+    bool                             m_revocationCheckEnabled; ///< Flag to enable certificate revocation checking.
+    std::string                      m_connectionLoggingName; ///< Name for logging connections.
 };
 
 }  // namespace impl
 }  // namespace tls
 }  // namespace vwg
-
 
 #endif  // SACCESSLIB_TLSCERTENGINE_HPP
