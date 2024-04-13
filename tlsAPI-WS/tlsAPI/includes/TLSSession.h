0bbd8b5781d33095abc63976850e021a2d2024b0
ohadi100 <136633247+ohadi100@users.noreply.github.com>
2024-04-14 02:24:12 +0300

    Update TLSSession.h

--- tlsAPI-WS/tlsAPI/includes/TLSSession.h
+++ tlsAPI-WS/tlsAPI/includes/TLSSession.h
@@ -1,308 +1,233 @@
 /**
+ * @file TLSSession.h
+ * @brief Defines the interfaces for managing TLS (Transport Layer Security) sessions.
  *
- * \copyright
+ * This file contains the interfaces and types necessary for managing the lifecycle of
+ * TLS sessions between a client and server, including sending, receiving data, and
+ * managing session status.
+ *
+ * @copyright
  * (c) 2022, 2023 CARIAD SE, All rights reserved.
  *
  * NOTICE:
+ * All information and materials herein, including intellectual and technical concepts,
+ * are proprietary to CARIAD SE, may be covered by patents, and protected by trade secret
+ * and copyright law.
  *
- * All the information and materials contained herein, including the
- * intellectual and technical concepts, are the property of CARIAD SE and may
- * be covered by patents, patents in process, and are protected by trade
- * secret and/or copyright law.
+ * Unauthorized reproduction, dissemination, modification, distribution, performance,
+ * display, or other use of this source code without prior written consent from CARIAD SE
+ * is strictly prohibited and may violate applicable laws.
  *
- * The copyright notice above does not evidence any actual or intended
- * publication or disclosure of this source code, which includes information
- * and materials that are confidential and/or proprietary and trade secrets of
- * CARIAD SE.
- *
- * Any reproduction, dissemination, modification, distribution, public
- * performance, public display of or any other use of this source code and/or
- * any other information and/or material contained herein without the prior
- * written consent of CARIAD SE is strictly prohibited and in violation of
- * applicable laws.
- *
- * The receipt or possession of this source code and/or related information
- * does not convey or imply any rights to reproduce, disclose or distribute
- * its contents or to manufacture, use or sell anything that it may describe
- * in whole or in part.
+ * Possession or receipt of this source code does not convey any rights to reproduce,
+ * disclose, or distribute its contents, or to manufacture, use, or sell anything it may
+ * describe, in whole or in part.
  */
 
 #ifndef SRC_TLSSESSION_H_
 #define SRC_TLSSESSION_H_
 
-
 #include <functional>
 #include <string>
 #include <memory>
-
 #include "TLSApiTypes.h"
 #include "vwgtypes.h"
 #include "TLSReturnCodes.h"
-#include "vwgtypes.h"
 
-using namespace vwg::types;
+namespace vwg {
+namespace tls {
 
-
-namespace vwg
-{
-namespace tls
-{
+/**
+ * @enum TLSDropStatus
+ * @brief Enumerates the possible states for dropping a TLS session.
+ */
 enum TLSDropStatus : UInt32 {
-    TLSDROP_SECURED,
-    TLSDROP_DROPPED,
-    TLSDROP_REQUESTED,
-    TLSDROP_SEND_LOCKED,
-    TLSDROP_PERFORMED
+    TLSDROP_SECURED,       ///< Connection is secured.
+    TLSDROP_DROPPED,       ///< TLS has been dropped, connection unsecured.
+    TLSDROP_REQUESTED,     ///< TLS drop has been requested.
+    TLSDROP_SEND_LOCKED,   ///< Sending is locked.
+    TLSDROP_PERFORMED      ///< TLS drop has been performed.
 };
 
 /**
- * \brief Defines the possible status values of the session.
+ * @enum TLSSessionStatus
+ * @brief Enumerates the states of a TLS session.
  */
 enum TLSSessionStatus : UInt32 {
-    /**
-     * \brief TLSSESSION_SECURED shall be the default case.
-     * This indicates that the connection is active an security is active.
-     */
-    TLSSESSION_SECURED,
-
-    /**
-     * \brief TLSSESSION_UNSECURED is only be supported in case the TLS can be dropped.
-     * This indicates that the connection is active but security was dropped.
-     */
-    TLSSESSION_UNSECURED,
-
-    /**
-     * \brief TLSSESSION_BROKEN indicates that a connection is not working anymore, due to errors.
-     */
-    TLSSESSION_BROKEN,
-
-    /**
-     * \brief TLSSESSION_CLOSED indicates that a connection is closed.
-     */
-    TLSSESSION_CLOSED
-
+    TLSSESSION_SECURED,    ///< Session is secured and active.
+    TLSSESSION_UNSECURED,  ///< Session is unsecured, TLS can be dropped.
+    TLSSESSION_BROKEN,     ///< Session is broken due to errors.
+    TLSSESSION_CLOSED      ///< Session is closed.
 };
 
 /**
- * \brief Defines the EOF value 0 in case that the connection is closed.
- * This can happen if a closed on a socket is made and there are pending receive and send.
- * Please be aware of that EOF is defined as -1
+ * @brief Represents EOF value for closed connections.
  */
-const int TLS_EOF = 0;
-
+constexpr int TLS_EOF = 0;
 
 class ITLSSessionEndpoint;
 
+/**
+ * @typedef SPITLSSessionEndpoint
+ * @brief Shared pointer to an ITLSSessionEndpoint interface.
+ */
 using SPITLSSessionEndpoint = std::shared_ptr<ITLSSessionEndpoint>;
 
-
 /**
- * \typedef This defines a C-style listener function to listen to the status changes of the session.
+ * @typedef TLSSessionStatusListener
+ * @brief Function type for listening to session status changes.
+ * @param endpoint The session endpoint.
+ * @param status The new session status.
  */
-using TLSSessionStatusListener = std::function<void(SPITLSSessionEndpoint endpoint, const TLSSessionStatus status)>;
-
+using TLSSessionStatusListener = std::function<void(SPITLSSessionEndpoint endpoint, TLSSessionStatus status)>;
 
 /**
- * \typedef This defines a listener function type to listen to the status changes.
+ * @typedef TLSDropStatusListener
+ * @brief Function type for listening to drop status changes.
+ * @param endpoint The session endpoint.
+ * @param status The new drop status.
  */
-using TLSDropStatusListener = std::function<void(SPITLSSessionEndpoint endpoint, const TLSDropStatus status)>;
+using TLSDropStatusListener = std::function<void(SPITLSSessionEndpoint endpoint, TLSDropStatus status)>;
 
 /**
- * \brief Represents a communication session between a service provider and a service consumer.
- * This interface must be implemented by the supplier.
+ * @class ITLSSessionEndpoint
+ * @brief Interface for managing TLS session endpoints.
  *
- *  Herewith one user can make send and receive data between the service provider and a service consumer
- *  The calls are basically blocking and will return until the operations is performed.
- *  This includes:
- *  - network operations.
- *  - Encrypting or decrypting data.
- *
+ * This interface is responsible for managing communication sessions that are secured by TLS.
+ * It provides methods to send and receive data securely, as well as to manage the state and configuration
+ * of the TLS session.
  */
-class ITLSSessionEndpoint : public ITLSSocketBase
-{
+class ITLSSessionEndpoint : public ITLSSocketBase {
 public:
     ITLSSessionEndpoint() = default;
-
     virtual ~ITLSSessionEndpoint() = default;
 
-
-public:
-    /* ------------ write functions--------------------- */
-
     /**
-     * \brief Sends a number of bytes from b[0] to b[len-1].
-     *
-     * \note The method blocks, unless in non-blocking mode.
-     * When an operation is repeated in non-blocking mode, it must be repeated with the same arguments.
-     *
-     * \param[in] b data buffer for sending data from it.
-     * \param[in] len buffer's length, in bytes
-     *
-     * \return the number of send bytes, or a negative value will indicate an error. The value 0 will
-     * indicated that the stream is closed (see TLS_EOF) Use getPendingErrors to retrieve the pending error.
+     * @brief Sends data over the TLS session.
+     * @param b Pointer to the data buffer.
+     * @param len Number of bytes to send.
+     * @return Number of bytes sent, 0 if the connection is closed, or a negative error code.
      */
     virtual Int32 send(const Byte b[], const Int32 len) = 0;
 
     /**
-     * \brief send a number of bytes from b[0+offset] to b[len-1] starting at b at given offset.
-     *
-     * \note The method blocks, unless in non-blocking mode.
-     * When an operation is repeated in non-blocking mode, it must be repeated with the same arguments.
-     *
-     * \param[in] b data buffer for sending data from it.
-     * \param[in] offset offset from the beginning of the buffer to send data from it.
-     * \param[in] len buffer's length, in bytes.
-     *
-     * \return the number send bytes, or a negative value will indicate an error. The value 0 will
-     * indicated that the stream is closed (see TLS_EOF) Use getPendingErrors to retrieve the pending error.
+     * @brief Sends data over the TLS session from a specified offset.
+     * @param b Pointer to the data buffer.
+     * @param offset Offset in the buffer from which to start sending data.
+     * @param len Number of bytes to send from the offset.
+     * @return Number of bytes sent, 0 if the connection is closed, or a negative error code.
      */
     virtual Int32 send(const Byte b[], const UInt32 offset, const Int32 len) = 0;
 
     /**
-     * \brief Forces to send the bytes.
-     * Depending on the underlying socket implementation, it can happen that bytes are still within the send buffer.
-     *
-     * \return 0 if no error had occurred, or a negative value will indicate an error. The value 0 will indicated that
-     * the stream is closed (see TLS_EOF) Use getPendingErrors to retrieve the pending error.
+     * @brief Forces any buffered data to be sent over the TLS session.
+     * @return 0 if successful, or a negative error code if an error occurred.
      */
     virtual Int32 flush() = 0;
 
-    /* ------------ read functions--------------------- */
-
     /**
-     * \brief Checks if bytes are available.
-     * The method blocks until data are available.
-     *
-     * \return the number of available bytes.
+     * @brief Checks how many bytes are available for reading.
+     * @return Number of bytes available to be read.
      */
     virtual Int32 available() = 0;
 
     /**
-     * \brief Receive up to len bytes from stream into the buffer starting at b.
-     *
-     * \note The method blocks until data are available, unless in non-blocking mode.
-     * In case of error use getPendingErrors to retrieve the pending error.
-     *
-     * \param[in] b buffer to be set with received date.
-     * \param[in] len buffer's length, in bytes.
-     *
-     * \return the number of received bytes, or a negative value will indicate an error. The value 0 will indicated
-     * that the stream is closed (see TLS_EOF).
+     * @brief Receives data from the TLS session.
+     * @param b Buffer to store the received data.
+     * @param len Maximum number of bytes to read.
+     * @return Number of bytes received, 0 if the connection is closed, or a negative error code.
      */
     virtual Int32 receive(Byte b[], const Int32 len) = 0;
 
     /**
-     * \brief Receive up to len bytes from stream into the buffer starting at b at given offset.
-     *
-     * \note The method blocks until data are available, unless in non-blocking mode.
-     *
-     * \param[in] b buffer to be set with received date.
-     * \param[in] offset offset from beginning of the buffer to set data from it.
-     * \param[in] len buffer's length, in bytes.
-     *
-     * \return the number of number of received, or a negative value will indicate an error. The value 0 will indicated
-     * that the stream is closed (see TLS_EOF) Use getPendingErrors to retrieve the pending error.
+     * @brief Receives data from the TLS session starting at a specific offset in the buffer.
+     * @param b Buffer to store the received data.
+     * @param offset Offset in the buffer at which to start storing data.
+     * @param len Maximum number of bytes to read.
+     * @return Number of bytes received, 0 if the connection is closed, or a negative error code.
      */
     virtual Int32 receive(Byte b[], const UInt32 offset, const Int32 len) = 0;
 
     /**
-     * \brief Sets blocking/non-blocking mode for the session. Blocking by default.
-     *
-     * \return success indication.
+     * @brief Sets the blocking mode of the session socket.
+     * @param blocking True to set the socket to blocking mode, false for non-blocking.
+     * @return Status code representing success or the specific error that occurred.
      */
     virtual TLSReturnCodes setBlocking(bool blocking) = 0;
 
     /**
-     * \brief Gets the network socket file descriptor.
-     *
-     * \return the network socket file descriptor.
+     * @brief Retrieves the underlying socket file descriptor.
+     * @return Network socket file descriptor.
      */
     virtual int getSocketFD() = 0;
 
     /**
-     * \brief Sends a "close notify" alert to the peer.
-     * The method blocks, unless in non-blocking mode.
-     *
-     * \return success indication.
+     * @brief Sends a shutdown notification to the peer and closes the session.
+     * @return Status code representing success or the specific error that occurred.
      */
     virtual TLSReturnCodes shutdown() = 0;
 
     /**
-     * \brief Gets the sSOA domain name of the session endpoint.
-     *
-     * \return the sSOA domain name of the session endpoint.
+     * @brief Retrieves the local domain name associated with the session endpoint.
+     * @return Local domain name as a string.
      */
     virtual std::string getLocalDomainName() = 0;
 
     /**
-     * \brief Gets the sSOA domain name of the remote session endpoint.
-     *
-     * \return the sSOA domain name of the remote session endpoint.
+     * @brief Retrieves the remote domain name associated with the session endpoint.
+     * @return Remote domain name as a string.
      */
     virtual std::string getRemoteDomainName() = 0;
 
     /**
-     * \brief Gets the port of the remote session endpoint .
-     *
-     * \return Gets the port of the remote session endpoint .
+     * @brief Retrieves the port number of the remote endpoint.
+     * @return Port number.
      */
     virtual UInt16 getRemotePort() = 0;
 
     /**
-     * \brief Gets the inet address of the remote session endpoint .
-     *
-     * \return Gets the inet address of the remote session endpoint .
+     * @brief Retrieves the internet address of the remote session endpoint.
+     * @return Shared pointer to an InetAddress object representing the remote address.
      */
     virtual SPIInetAddress getRemoteInetAddress() = 0;
 
 #ifdef TLSAPI_WITH_DROP_SUPPORT
     /**
-     * \brief Herewith it it is possible to ask if the session end-point will support TLS dropping or not.
-     * It can be that the server will not support dropping, which is not know statically by the client,
-     * this is only the convenience method to check if the socket was created droppable or not.
-     *
-     * \return true when created droppable.
+     * @brief Checks if the session endpoint supports TLS dropping.
+     * @return True if dropping is supported, false otherwise.
      */
     virtual Boolean isDroppable() = 0;
 
     /**
-     * \brief this is a special method to stop sending and receiving encrypted data over the same connection.
-     * Basically there are two versions of the library, one which will support the drop feature and one without drop
-     * feature. This is important that the drop feature is not provided for the normal use case, where it not permitted
-     * to use drop TLS feature.
-     *
-     * \return success indication.
+     * @brief Initiates the process to drop TLS protection for the session.
+     * @return Status code representing success or the specific error that occurred.
      */
     virtual TLSReturnCodes dropTLS() = 0;
 #endif
 
     /**
-     * \brief Gets the current TLS drop status.
-     *
-     * \return the current TLS drop status of the connection.
+     * @brief Retrieves the current drop status of the session.
+     * @return Current drop status as a TLSDropStatus enum value.
      */
     virtual TLSDropStatus getDropState() = 0;
 
-
     /**
-     * \brief Sets the listener function (C++-style) for status changes of the session.
-     * This overwrites the listener when already set.
-     *
-     * \param[in] listener listener function to be set.
+     * @brief Sets a listener for session status changes.
+     * @param listener Function to be called when the session status changes.
      */
     virtual void setSessionStatusListener(TLSSessionStatusListener listener) = 0;
 
     /**
-     * \brief Sets the  listener function (C++ -style) for drop changes of the session.
-     * this overwrites the listener when already set.
+     * @brief Sets a listener for TLS drop status changes.
+     * @param listener Function to be called when the drop status changes.
      */
     virtual void setDropStatusListener(TLSDropStatusListener listener) = 0;
 };
 
-using SPTLSSessionEndpoint     = std::shared_ptr<ITLSSessionEndpoint>;
+using SPTLSSessionEndpoint = std::shared_ptr<ITLSSessionEndpoint>;
 using TLSSessionEndpointResult = TLSResult<SPTLSSessionEndpoint>;
 
-} /* namespace tls */
-} /* namespace vwg */
 
-#endif /* SRC_TLSSESSION_H_ */
+} // namespace tls
+} // namespace vwg
+
+#endif // SRC_TLSSESSION_H_
