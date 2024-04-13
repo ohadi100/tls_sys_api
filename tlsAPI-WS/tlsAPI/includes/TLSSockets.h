b94aa35a1f32e3aaecf348f12f01a4bece1004ed
ohadi100 <136633247+ohadi100@users.noreply.github.com>
2024-04-14 02:32:32 +0300

    Update TLSSockets.h

--- tlsAPI-WS/tlsAPI/includes/TLSSockets.h
+++ tlsAPI-WS/tlsAPI/includes/TLSSockets.h
@@ -1,37 +1,27 @@
 /**
+ * @file TLSSockets.h
+ * @brief Interface definitions for TLS-aware sockets.
  *
- * \copyright
+ * This file provides the interface definitions for TLS server and client sockets.
+ * These interfaces are used to establish and manage secure TLS or TLS-PSK connections over the network.
+ *
+ * @copyright
  * (c) 2022, 2023 CARIAD SE, All rights reserved.
  *
  * NOTICE:
- *
- * All the information and materials contained herein, including the
- * intellectual and technical concepts, are the property of CARIAD SE and may
- * be covered by patents, patents in process, and are protected by trade
+ * All information and materials contained herein, including intellectual and technical concepts,
+ * are the property of CARIAD SE and may be covered by patents, patents in process, and are protected by trade
  * secret and/or copyright law.
  *
- * The copyright notice above does not evidence any actual or intended
- * publication or disclosure of this source code, which includes information
- * and materials that are confidential and/or proprietary and trade secrets of
- * CARIAD SE.
- *
- * Any reproduction, dissemination, modification, distribution, public
- * performance, public display of or any other use of this source code and/or
- * any other information and/or material contained herein without the prior
- * written consent of CARIAD SE is strictly prohibited and in violation of
- * applicable laws.
- *
- * The receipt or possession of this source code and/or related information
- * does not convey or imply any rights to reproduce, disclose or distribute
- * its contents or to manufacture, use or sell anything that it may describe
- * in whole or in part.
+ * Reproduction, dissemination, modification, distribution, public performance, public display,
+ * or any other use of this source code and/or any other information and/or material contained herein
+ * without the prior written consent of CARIAD SE is strictly prohibited and in violation of applicable laws.
  */
 
 #ifndef SRC_TLSSOCKETS_H_
 #define SRC_TLSSOCKETS_H_
 
 #include <memory>
-
 #include "vwgtypes.h"
 #include "TLSApiTypes.h"
 #include "TLSResult.h"
@@ -39,116 +29,89 @@
 
 using namespace vwg::types;
 
-namespace vwg
-{
-namespace tls
-{
+namespace vwg {
+namespace tls {
+
 /**
- * \brief Server TLS-PSK aware server socket interface.
- * This interface must be implemented by the supplier.
+ * @class ITLSServerSocket
+ * @brief Interface for TLS-PSK aware server sockets.
  *
- * For TCP based communication make an accept call to retrieve a connection to the client.
- * The client connection is represented by a TLSSession where one can read and write the data.
- * Within the accept call all needed operations are performed.
- * This includes:
- *  - accept the network connection
- *  - make the TLS or TLS-PSK handshake (see https://tools.ietf.org/html/rfc4279)
- *  - derive the pre shared key from the SSOA domain name
- *  - derive the session key from the pre shared key stored within the trust zone.
+ * This interface defines the necessary methods for managing server-side TLS sockets, including
+ * accepting incoming connections and performing necessary TLS handshakes.
  */
-class ITLSServerSocket : public ITLSSocketBase
-{
+class ITLSServerSocket : public ITLSSocketBase {
 public:
-    ITLSServerSocket()          = default;
+    ITLSServerSocket() = default;
     virtual ~ITLSServerSocket() = default;
 
-public:
     /**
-     * \brief This is a blocking call for the server implementation to wait until the client will get a connection.
-     * The server may fork several thread to handle each client in an individual thread.
-     * This accept covers all needed operations like
-     *  - accept the network connection
-     *  - make the TLS or TLS-PSK handshake (see https://tools.ietf.org/html/rfc4279)
-     *  - derive the pre shared key from the SSOA domain name
-     *  - derive the session key from the pre shared key stored within the trust zone.
+     * @brief Accepts an incoming connection and performs a TLS handshake.
      *
-     * \return a ITLSSessionEndpoint instance when operation was successful, otherwise an error code is delivered.
+     * This is a blocking call that waits for a client to establish a connection.
+     * It performs the necessary network operations and TLS or TLS-PSK handshake as defined in RFC 4279.
+     *
+     * @return A TLSSessionEndpointResult containing either a successful session endpoint or an error code.
      */
     virtual TLSSessionEndpointResult accept() = 0;
 
     /**
-     * \brief Sets the socket timeout.
+     * @brief Sets the socket timeout for operations on this socket.
      *
-     * \param[in] timeout the new socket timeout value in milliseconds.
+     * @param timeout The timeout value in milliseconds.
      */
     virtual void setSoTimeout(Int32 timeout) = 0;
 
     /**
-     * \brief Gets the network socket file descriptor.
+     * @brief Retrieves the network socket file descriptor.
      *
-     * \return the network socket file descriptor.
+     * @return The socket file descriptor.
      */
     virtual int getSocketFD() = 0;
 };
 
-
 /**
- * \brief Server TLS-PSK aware client socket interface.
- * This interface must be implemented by the supplier.
+ * @class ITLSClientSocket
+ * @brief Interface for TLS-PSK aware client sockets.
  *
- * For TCP based communication make a connect call to retrieve a connection to the server.
- * The server connection is represented by a TLSSession where one can read and write the data.
- * Within the connect call all needed operations are performed.
- * This includes:
- *  - make the TLS or TLS-PSK handshake (see https://tools.ietf.org/html/rfc4279).
- *  - derive the pre shared key from the SSOA domain name.
- *  - derive the session key from the pre shared key stored within the trust zone.
- *
+ * This interface defines the necessary methods for managing client-side TLS sockets, including
+ * connecting to a server and performing necessary TLS handshakes.
  */
-class ITLSClientSocket : public ITLSSocketBase
-{
+class ITLSClientSocket : public ITLSSocketBase {
 public:
     ITLSClientSocket() = default;
-
     virtual ~ITLSClientSocket() = default;
 
-
-public:
     /**
-     * \brief a client shall call this method in to get connected to the server.
-     * This will do all underling operations like
-     *  - make the TLS or TLS-PSK handshake (see https://tools.ietf.org/html/rfc4279)
-     *  - derive the pre shared key  from the SSOA domain name
-     *  - derive the session key from the pre shared key stored within the trust zone.
+     * @brief Connects to a server and performs a TLS handshake.
      *
-     *  \return an ITLSSessionEndpoint instance when operation was successful, otherwise an error code is delivered.
+     * This method handles the underlying operations required to establish a secure connection,
+     * including performing a TLS or TLS-PSK handshake as per RFC 4279.
+     *
+     * @return A TLSResult containing either a successful session endpoint or an error code.
      */
     virtual TLSResult<std::shared_ptr<ITLSSessionEndpoint>> connect() = 0;
 
     /**
-     * \brief Changes the default socket timeout, SO_RCVTIMEO and SO_SNDTIMEO options,
-     * according to https://linux.die.net/man/3/setsockopt.
+     * @brief Sets the socket timeout for receiving and sending operations.
      *
-     * \param[in] timeout The new socket timeout value in milliseconds.
+     * @param timeout The timeout value in milliseconds.
      */
     virtual void setSoTimeout(Int32 timeout) = 0;
 
     /**
-     * \brief Gets the network socket file descriptor.
+     * @brief Retrieves the network socket file descriptor.
      *
-     * \return the network socket file descriptor.
+     * @return The socket file descriptor.
      */
     virtual int getSocketFD() = 0;
 };
 
-
-using SPTLSClientSocket     = std::shared_ptr<ITLSClientSocket>;
-using SPTLSServerSocket     = std::shared_ptr<ITLSServerSocket>;
+using SPTLSClientSocket = std::shared_ptr<ITLSClientSocket>;
+using SPTLSServerSocket = std::shared_ptr<ITLSServerSocket>;
 using TLSClientSocketResult = TLSResult<SPTLSClientSocket>;
 using TLSServerSocketResult = TLSResult<SPTLSServerSocket>;
 
-
-} /* namespace tls */
-} /* namespace vwg */
+} // namespace tls
+} // namespace vwg
 
 #endif /* SRC_TLSSOCKETS_H_ */
