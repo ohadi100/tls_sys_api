4816e71e0f6f831e89f8d0bcf69ee4f7fffd4d28
ohadi100 <136633247+ohadi100@users.noreply.github.com>
2024-04-14 20:30:55 +0300

    Update Logger.hpp

--- tlsAPI-WS/tlsLibImpl/common/include/Logger.hpp
+++ tlsAPI-WS/tlsLibImpl/common/include/Logger.hpp
@@ -1,4 +1,12 @@
-/* Copyright (c) 2023 Volkswagen Group */
+/**
+ * @file Logger.hpp
+ * @copyright (c) 2023 Volkswagen Group
+ *
+ * @brief Provides a logging utility for the TLS library, encapsulating different logging mechanisms for
+ * production and unit testing environments.
+ *
+ * Detailed explanation of copyright and permission notices.
+ */
 
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
@@ -6,25 +14,31 @@
 #ifndef UNIT_TEST
 #include "ara/log/logging.h" 
 #else
-#include <time.h>
-#include <string.h>
+#include <ctime>
+#include <cstring>
 #include <iostream>
 #endif
+
 namespace vwg
 {
 namespace tls
 {
 namespace impl
 {
-
 #ifndef UNIT_TEST
 
+/**
+ * @brief Retrieves a static instance of ara::log::Logger specific for the TLS module.
+ * 
+ * @return Reference to the ara::log::Logger instance.
+ */
 inline ara::log::Logger& getLogger() noexcept 
 {
     static ara::log::Logger& logger = ara::log::CreateLogger("TLS", "TLS library");
     return logger;
 }
 
+/// Macros for various log levels using ARA log
 #define FND_LOG_VERBOSE vwg::tls::impl::getLogger().LogVerbose()
 #define FND_LOG_DEBUG vwg::tls::impl::getLogger().LogDebug()
 #define FND_LOG_INFO vwg::tls::impl::getLogger().LogInfo()
@@ -34,15 +48,26 @@ inline ara::log::Logger& getLogger() noexcept
 
 #else
 
+/**
+ * @brief Helper function to get current time as a string.
+ * 
+ * @return Current system time formatted as a string.
+ */
 static inline char *timenow();
 
-#define FND_LOG_VERBOSE std::cout << vwg::tls::impl::timenow() << "  Verbose: " 
+/// Macros for various log levels using standard output for unit testing
+#define FND_LOG_VERBOSE std::cout << vwg::tls::impl::timenow() << "  Verbose: "
 #define FND_LOG_DEBUG std::cout << vwg::tls::impl::timenow() << "  Debug: "
 #define FND_LOG_INFO std::cout << vwg::tls::impl::timenow() << "  Info: "
 #define FND_LOG_WARN std::cout << vwg::tls::impl::timenow() << "  * Warning: * "
 #define FND_LOG_ERROR std::cout << vwg::tls::impl::timenow() << "  *** Error: *** "
 #define FND_LOG_FATAL std::cout << vwg::tls::impl::timenow() << "  *** Fatal: *** "
 
+/**
+ * @brief Implements the helper function timenow to fetch current system time formatted as a string.
+ * 
+ * @return Pointer to a character array containing the formatted date and time.
+ */
 static inline char *timenow() {
     static char buffer[64];
     time_t rawtime;
