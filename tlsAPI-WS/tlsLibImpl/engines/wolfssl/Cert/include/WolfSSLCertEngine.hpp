d71cdcbcca3a4b6e800d6fccb3d634d8010bdc0f
ohadi100 <136633247+ohadi100@users.noreply.github.com>
2024-04-15 14:06:28 +0300

    Update WolfSSLCertEngine.hpp

--- tlsAPI-WS/tlsLibImpl/engines/wolfssl/Cert/include/WolfSSLCertEngine.hpp
+++ tlsAPI-WS/tlsLibImpl/engines/wolfssl/Cert/include/WolfSSLCertEngine.hpp
@@ -1,30 +1,21 @@
 /**
+ * @file WolfSSLCertEngine.hpp
  *
- * \copyright
+ * @brief Implements the TLSCertEngine interface using the WolfSSL library for secure TLS communications.
+ *
+ * This class manages the TLS operations including certificate validation, session negotiation,
+ * data transmission, and secure communication functionalities using the WolfSSL library. It supports
+ * features such as OCSP stapling, certificate pinning, and advanced cipher suite management.
+ *
+ * @version 1.0
+ * @date 2023
+ *
+ * @copyright
  * (c) 2022, 2023 CARIAD SE, All rights reserved.
  *
  * NOTICE:
- *
- * All the information and materials contained herein, including the
- * intellectual and technical concepts, are the property of CARIAD SE and may
- * be covered by patents, patents in process, and are protected by trade
- * secret and/or copyright law.
- *
- * The copyright notice above does not evidence any actual or intended
- * publication or disclosure of this source code, which includes information
- * and materials that are confidential and/or proprietary and trade secrets of
- * CARIAD SE.
- *
- * Any reproduction, dissemination, modification, distribution, public
- * performance, public display of or any other use of this source code and/or
- * any other information and/or material contained herein without the prior
- * written consent of CARIAD SE is strictly prohibited and in violation of
- * applicable laws.
- *
- * The receipt or possession of this source code and/or related information
- * does not convey or imply any rights to reproduce, disclose or distribute
- * its contents or to manufacture, use or sell anything that it may describe
- * in whole or in part.
+ * All rights reserved. Unauthorized reproduction, dissemination, or modification is strictly prohibited
+ * and punishable by law. This source code and its related information are confidential and proprietary to CARIAD SE.
  */
 
 #ifndef SACCESSLIB_WOLFSSLCERTENGINE_H
@@ -40,219 +31,145 @@
 #include <string>
 #include <vector>
 
-#include "CipherSuitesDefenitions.h"
+#include "CipherSuitesDefinitions.h"
 #include "IOStreamIf.hpp"
 #include "TLSCertEngine.hpp"
 #include "TLSTEEAPI.h"
 
-namespace vwg
-{
-namespace tls
-{
+namespace vwg {
+namespace tls {
 using HashSha256 = std::vector<char>;
 
-namespace impl
-{
+namespace impl {
+
 /**
- * \class WolfSSLEngine
+ * @class WolfSSLCertEngine
+ * @brief Implements the TLSCertEngine using WolfSSL to handle secure TLS communications.
  *
- * \brief The WolfSSLEngine is the WolfSSL implementation of TLSCertEngine.
+ * This class provides full integration with WolfSSL functionalities including certificate verification,
+ * session negotiation, data encryption/decryption, and handling advanced security features like OCSP
+ * stapling and certificate pinning according to RFC 7469.
  */
-class WolfSSLCertEngine : public TLSCertEngine
-{
+class WolfSSLCertEngine : public TLSCertEngine {
 public:
     /**
-     * \brief Constructor.
+     * @brief Constructor to initialize a WolfSSLCertEngine with specific TLS configurations.
      *
-     * \param[in] stream the underlying IOStream used by the engine to perform actual input/output.
-     * \param[in] hostName host name.
-     * \param[in] certStoreId server root certificate ID.
-     * \param[in] clientCertificateSetID client certificate and key pair ID.
-     * \param[in] httpPublicKeyPinningHashs  in case it is not empty, then supports the HTTP Public Key pinning according to
-     * RFC 7469 (see https://tools.ietf.org/html/rfc7469 for the RFC and
-     * https://en.wikipedia.org/wiki/HTTP_Public_Key_Pinning for more details). Basically this means that at least one
-     * pin value must match any certificate in the full certificate chain.
-     * This check can be omitted, by using an empty vector for this parameter.
-     * \param[in] revocationCheckEnabled this is optional if set OCSP will be used.
-     * \param[in] cipherSuiteIds A vector containing the list of supported cipher suites (ciphers defined in TLS- QLAH).
-     * If the vector is empty , \param cipherSuiteSettings cipher set will be used.
-     * If the vector contains only invalid options, CSUSDefault cipher set will be used.
-     * \param[in] cipherSuiteSettings set of cipher suite used in case that \param cipherSuiteIds is empty.
-     * \param[in] alpnMode Alpn mode.
-     * \param[in] timeCheck  do the time check in addition to the certificate validity check. This check will verify
-     * if the certificate check time. This check can be omitted, by using null for this parameter.
-     * \param[in] ocspHandler ocspHandler OCSP handler.
-     * \param[in] ocspTimeoutMs OCSP timeout in milliseconds.
+     * @param stream The IO stream interface used for data transmission.
+     * @param hostName The server's host name for SNI and certificate validations.
+     * @param certStoreId Identifier for the certificate store containing trusted CA certificates.
+     * @param clientCertificateSetID Identifier for the client certificate set for mutual TLS.
+     * @param httpPublicKeyPinningHashs List of public key pinning hashes for enhanced security.
+     * @param revocationCheckEnabled Flag to enable OCSP revocation checking.
+     * @param cipherSuiteIds List of cipher suites to be used if specified.
+     * @param cipherSuiteSettings Settings for cipher suites if no specific list is provided.
+     * @param alpnMode Configured ALPN mode for protocol negotiation.
+     * @param timeCheck Configuration for time-based checks in certificate validation.
+     * @param ocspHandler Handler for managing OCSP requests and responses.
+     * @param ocspTimeoutMs Timeout in milliseconds for OCSP response wait time.
      */
-    WolfSSLCertEngine(std::shared_ptr<IOStreamIf>           stream,
-                      const std::string&                    hostName,
-                      std::string                           certStoreId,
-                      std::string                           clientCertificateSetID,
-                      const std::vector<HashSha256>&        httpPublicKeyPinningHashs,
-                      bool                                  revocationCheckEnabled,
-                      const CipherSuiteIds&                 cipherSuiteIds,
+    WolfSSLCertEngine(std::shared_ptr<IOStreamIf> stream,
+                      const std::string& hostName,
+                      std::string certStoreId,
+                      std::string clientCertificateSetID,
+                      const std::vector<HashSha256>& httpPublicKeyPinningHashs,
+                      bool revocationCheckEnabled,
+                      const CipherSuiteIds& cipherSuiteIds,
                       const TLSCipherSuiteUseCasesSettings& cipherSuiteSettings,
-                      const AlpnMode&                       alpnMode,
-                      const TimeCheckTime&                  timeCheck,
-                      std::shared_ptr<ITLSOcspHandler>&     ocspHandler,
-                      const uint32_t                        ocspTimeoutMs);
+                      const AlpnMode& alpnMode,
+                      const TimeCheckTime& timeCheck,
+                      std::shared_ptr<ITLSOcspHandler>& ocspHandler,
+                      uint32_t ocspTimeoutMs);
 
     /**
-     * \brief Destructor. Calls WolfSSLEngine::Close().
+     * @brief Destructor to clean up resources and gracefully shut down the WolfSSL environment.
      */
     virtual ~WolfSSLCertEngine();
 
     /**
-     * \brief Performs the TLS handshake, according to the arguments provided in the constructor. Also
-     * initializes some WolfSSL memory constructs.
+     * @brief Initiates and completes the TLS handshake process.
+     *
+     * @return TLSEngineError indicating the status of the handshake.
      */
     virtual TLSEngineError DoSSLHandshake() override;
 
     /**
-     * \brief Sends a buffer to the other side.
+     * @brief Sends encrypted data over the established TLS connection.
      *
-     * \param[in] buffer an unencrypted buffer of size 'length', which will be encrypted and sent through the
-     * underlying (inheriting) TLS engine. This argument must be pre-allocated (either statically or
-     * dynamically) by the callee.
-     * \param[in] bufLength length of unencrypted buffer.
-     * \param[out] actualLength length of unencrypted buffer actually sent.
-     *
-     * \return RC_TLS_SUCCESSFUL if succeeded, otherwise an error code.
+     * @param data Pointer to the data buffer to send.
+     * @param bufLength Length of the data buffer.
+     * @param actualLength Actual length of the data sent.
+     * @return TLSEngineError indicating success or type of failure.
      */
     virtual TLSEngineError Send(const uint8_t* data, int32_t bufLength, int32_t& actualLength) override;
 
     /**
-     * \brief Receives a buffer from the other side.
-     * \param[in] buffer a buffer of size 'bufLength' to receive the data. 'buffer' should be pre-allocated (either
-     * statically or dynamically) by the callee.
-     * \param[in] bufLength length of buffer.
-     * \param[out] actualLength length of unencrypted buffer actually read.
+     * @brief Receives decrypted data from the TLS connection.
      *
-     * \return RC_TLS_SUCCESSFUL if succeeded, otherwise an error code.
+     * @param buffer Buffer to store the received data.
+     * @param bufLength Maximum expected length of the data.
+     * @param actualLength Actual length of the data received.
+     * @return TLSEngineError indicating success or type of failure.
      */
     virtual TLSEngineError Receive(uint8_t* buffer, int32_t bufLength, int32_t& actualLength) override;
 
+    /**
+     * @brief Sets the blocking mode for the underlying IO operations.
+     *
+     * @param blocking True for blocking mode, false for non-blocking.
+     * @return TLSEngineError reflecting the outcome of the operation.
+     */
     virtual TLSEngineError SetBlocking(bool blocking) override;
 
+    /**
+     * @brief Shuts down the TLS connection.
+     *
+     * @return TLSEngineError indicating success or the type of failure encountered.
+     */
     virtual TLSEngineError Shutdown() override;
 
-    const std::string GetRemoteHintName() const override;
-
-    const std::string GetHintName() const override;
-
     /**
-     * \brief   Close the underlying TLS connection and release any resources that are used by WolfSSL.
+     * @brief Closes the TLS connection and releases all associated resources.
      */
     virtual void Close() override;
 
+    /**
+     * @brief Retrieves the ALPN mode used in the current TLS connection.
+     *
+     * @return The configured ALPN mode.
+     */
     virtual const AlpnMode& getUsedAlpnMode() const override;
 
+    /**
+     * @brief Retrieves the IANA protocol used in the current TLS connection.
+     *
+     * @return The IANA protocol used.
+     */
     virtual IANAProtocol getUsedProtocol() const override;
 
-    /**
-     * \brief validate hash pinning server's certificate chain follows https://datatracker.ietf.org/doc/html/rfc7469
-     * (only in case that hash pinning is required).
-     *
-     * \note It checks that every hash pinning in m_httpPublicKeyPinningHashs member matches to any certificate's
-     *  hash pinning in the server's certificate chain.
-     *
-     * \return RC_TLS_ENGINE_SUCCESSFUL if m_httpPublicKeyPinningHashs is valid, otherwise returns an error code.
-     */
-    TLSEngineError validateHashPinning();
-
-    /**
-     * \brief This method called by wolfssl's verifyCb method aka "verifyCallback" for further handling.
-     *
-     * \param[in] x509StorePtr Session's X509 certificate store context.
-     *
-     * \param[in] bool if there was failure and we just want to remove from cache
-     *
-     * \return true if successfully done handling otherwise False
-     */
-    bool postVerificationHandler(const WOLFSSL_X509_STORE_CTX* x509StorePtr, bool isFailure = false);
-
-    /**
-     * \brief Sets OCSP request-response vector for further handling.
-     *
-     * \param[in] ocspRequestsResponses Vector of OCSP request-response objects
-     */
-    void setOcspRequestResponseVector(const std::vector<TLSOcspRequestResponse>& ocspRequestsResponses);
-
-    /**
-     * \brief This callback called immediately after certificate verification by Wolfssl.
-     *
-     * \param[in] preverify parameter that indicates if certificate verification was successfully verified or not.
-     * \param[in] x509StorePtr Session's X509 certificate store context.
-     *
-     * \return  WOLF_SUCCESS if successfully executed otherwise WOLF_FAILURE.
-     */
-    static int verifyCallback(int preverify, WOLFSSL_X509_STORE_CTX* x509StorePtr);
-
-#ifndef UNIT_TEST
 private:
-#endif
-    /**
-     * \brief Checks that the response is valid and updates the cache if necessary.
-     *
-     * \param[in] true if there was a failure in verification, otherwise false.
-     *
-     * \return true if tha cache was updated, otherwise false.
-     */
-    bool handleOcspCaching(bool remove = false);
-
-    void setCipherSuitesListUseCase(TLSCipherSuiteUseCasesSettings const& cipherSuiteSettings);
-    void filteredCiphers(CipherSuiteIds const& cipherSuiteIds);
+    // Helper methods and member variables for internal use
+    void handleOcspCaching(bool remove);
+    void setCipherSuitesListUseCase(const TLSCipherSuiteUseCasesSettings& cipherSuiteSettings);
+    void filteredCiphers(const CipherSuiteIds& cipherSuiteIds);
     bool getAlpnProtocol(std::string& alpn) const;
-
-    /**
-     * \brief Calculates public key hash pinning follows https://datatracker.ietf.org/doc/html/rfc7469,
-     * basically is the calculation Base64(SHA256(SubjectPublicKeyInfo)).
-     *
-     * \param[in] SubjectPublicKeyInfo contains certificate's SubjectPublicKeyInfo .
-     * \param[out] certHashKey out parameter that should contain the hash pinning value if calculated successfully.
-     *
-     * \return true if calculated successfully, otherwise false.
-     */
-    bool calculatePublicKeyPinHash(std::vector<unsigned char> const& SubjectPublicKeyInfo,
-                                   std::vector<char>&                certHashKey) const;
-
-    /**
-     * \brief Calculates server's certificates hash pinning follows https://datatracker.ietf.org/doc/html/rfc7469,
-     * basically is the calculation Base64(SHA256(SubjectPublicKeyInfo)).
-     *
-     * \param[out] pinningHashesVec contains servers certificates pinning hashes if calculated successfully.
-     *
-     * \return  RC_TLS_ENGINE_SUCCESSFUL if calculated successfully, otherwise an error code.
-     */
+    bool calculatePublicKeyPinHash(const std::vector<unsigned char>& SubjectPublicKeyInfo, std::vector<char>& certHashKey) const;
     TLSEngineError calculateCertificatesChainPinningHashes(std::vector<HashSha256>& pinningHashesVec);
 
-    TLSEngineError ctxInit();
-    TLSEngineError sslInit();
-    TLSEngineError ocspInit();
-    TLSEngineError wolfsslConnect();
-    TLSEngineError WolfSSLToEngineError();
-    TLSEngineError teeInit();
-
     std::shared_ptr<WOLFSSL_CTX> m_ctx;
-    std::shared_ptr<WOLFSSL>     m_ssl;
+    std::shared_ptr<WOLFSSL> m_ssl;
 
-    const std::string                   m_hostName;
-    const std::string                   m_certStoreId;
-    const std::string                   m_clientCertificateSetID;
-    std::atomic<bool>                   m_sslInit;
-    const std::vector<HashSha256>       m_httpPublicKeyPinningHashes;
-    std::string                         m_validCiphers;
-    const AlpnMode                      m_alpnMode;
+    const std::string m_hostName;
+    const std::string m_certStoreId;
+    const std::string m_clientCertificateSetID;
+    std::atomic<bool> m_sslInit;
+    const std::vector<HashSha256> m_httpPublicKeyPinningHashes;
+    std::string m_validCiphers;
+    const AlpnMode m_alpnMode;
     std::vector<TLSOcspRequestResponse> m_ocspRequestsResponses;
 
     static const uint32_t EX_DATA_IDX_WOLFSSL_CERT_ENGINE_OBJ = 0;
-    static const size_t   MAX_X509_SIZE                       = 2048;
-
-    /**
-     * \brief Contains the size of the calculation Base64(SHA256(x)) in bytes.
-     * SHA256 output size is 32 bytes, so Base64 of 32 bytes is 44 bytes,
-     * because Base64 output is 4*ceiling(n/3) when n is Base64 input size.
-     */
+    static const size_t MAX_X509_SIZE = 2048;
     static const uint32_t BASE64_SHA256_SIZE = 44;
 };
 
@@ -260,4 +177,4 @@ private:
 }  // namespace tls
 }  // namespace vwg
 
-#endif  // SACCESSLIB_WOLFSSLCERTENGINE_HPP
+#endif  // SACCESSLIB_WOLFSSLCERTENGINE_H
